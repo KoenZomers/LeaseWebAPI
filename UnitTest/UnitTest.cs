bd5471876e748adc94513315b86bc538ea45a5c4
Koen Zomers <mail@koenzomers.nl>
2015-05-27 10:47:18 +0200

    Added some test changed

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -30,6 +30,9 @@ namespace UnitTest
             _leaseWebServerId = ConfigurationManager.AppSettings["LeaseWebServerID"];
         }
 
+        /// <summary>
+        /// test
+        /// </summary>
         [TestMethod]
         public void GetDataTrafficTestMethod()
         {
